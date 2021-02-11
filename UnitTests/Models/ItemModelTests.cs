41a2a0bf54b53a3f9dacccc369de1063d0c4ee67
Joe Fedden <jfedden@gmail.com>
2021-02-10 17:13:54 -0800

    Added ItemModel_Get UT to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -10,13 +10,47 @@ namespace UnitTest.Models
         public void ItemModel_Constructor_Valid_Default_Should_Pass()
         {
             // Arrange
-
+            
             // Act
             var result = new ItemModel();
             // Reset
 
             //Assert
             Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+            // Reset
+
+            //Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+           
+            // Reset
+
+            //Assert
+            Assert.AreEqual(0, result.Value);
         }
     }
 }
