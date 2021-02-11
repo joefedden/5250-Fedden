404044a0894d9593fa587a7853c69d2059595116
Joe Fedden <jfedden@gmail.com>
2021-02-10 19:18:37 -0800

    Added UT for Invalid Dice 1, 0 returns 0

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -66,6 +66,20 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_InValid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
         public void RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5()
         {
             // Arrange
